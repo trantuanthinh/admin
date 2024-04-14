6826675441a82423c773a8bd36963c6f3f861f99
ngminhnguyen <minhnguyen2811200909@gmail.com>
2024-04-14 13:56:47 +0700

    nguyen

---
+++ src/app/admin/ingredient-management/ingredient-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ingredient-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './ingredient-management.component.html',
+  styleUrl: './ingredient-management.component.scss'
+})
+export class IngredientManagementComponent {
+
+}
---
+++ src/app/admin/order-management/order-management.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-management.component.html',
+  styleUrl: './order-management.component.scss'
+})
+export class OrderManagementComponent {
+
+}
