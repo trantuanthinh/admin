67b756fcdb375723495e5d284f1932a0180684af
trantuanthinh <tran.tuan.thinh.0125@gmail.com>
2024-05-07 16:44:00 +0700

    commit simple base

--- src/app/admin/body/customer-management/customer-management.component.ts
+++ src/app/admin/body/customer-management/customer-management.component.ts
@@ -9,6 +9,7 @@ import { MatSort, MatSortModule } from "@angular/material/sort";
 import { MatTableDataSource, MatTableModule } from "@angular/material/table";
 import { take } from "rxjs";
 import { ShareService } from "../../../shared/share.service";
+import { SimpleBaseComponent } from "../../../shared/simple.base.component";
 import { SharePropertyService } from "./../../../shared/share-property.service";
 import { CustomerManagementInfoComponent } from "./customer-management-info/customer-management-info.component";
 
@@ -37,11 +38,10 @@ export interface UserData {
     templateUrl: "./customer-management.component.html",
     styleUrl: "./customer-management.component.scss",
 })
-export class CustomerManagementComponent {
+export class CustomerManagementComponent extends SimpleBaseComponent {
     @ViewChild(MatPaginator) paginator!: MatPaginator;
     @ViewChild(MatSort) sort!: MatSort;
     displayedColumns: string[] = ["id", "firstName", "lastName", "phone", "email", "gender", "birthday", "action"];
-    public offset: number = 7;
     dataSource!: MatTableDataSource<any>;
     myform!: FormGroup<any>;
 
@@ -50,6 +50,7 @@ export class CustomerManagementComponent {
         private shareService: ShareService,
         private sharePropertyService: SharePropertyService
     ) {
+        super();
         this.getData();
     }
 
